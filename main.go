f3b2c0c5b6713761afea6679bc83fd160c47ff85
tentakle <tentakle@users.noreply.github.com>
2021-03-06 00:09:55 +0300

    fix

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
 import (
-	"crypto/tls"
-	"flag"
+	//"crypto/tls"
+	"encoding/json"
+	//"flag"
 	"fmt"
+	//"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/route53"
 	"log"
+	//"os"
 )
 
 func main() {
-	wordPtr := flag.String("word", "foo", "a string")
-	flag.Parse()
-	fmt.Println("word:", *wordPtr)
+	/*
+		wordPtr := flag.String("word", "foo", "a string")
+		flag.Parse()
+		fmt.Println("word:", *wordPtr)
+	*/
+	type Domains struct {
+		Targets []string `json:"targets"`
+	}
+
+	arr := []Domains{
+		Domains{
+			Targets: []string{},
+		},
+	}
 
 	sess := session.Must(session.NewSession())
 
@@ -24,7 +38,7 @@ func main() {
 			pageNum++
 
 			for _, zone := range page.HostedZones {
-				fmt.Printf("Hosted Zone: %v\n\n", *zone.Name)
+				// fmt.Printf("\nHosted Zone: %v\n\n", *zone.Name)
 				pageNum := 0
 
 				err := svc.ListResourceRecordSetsPages(&route53.ListResourceRecordSetsInput{
@@ -32,31 +46,58 @@ func main() {
 				},
 					func(page *route53.ListResourceRecordSetsOutput, lastPage bool) bool {
 						pageNum++
-
 						for _, record := range page.ResourceRecordSets {
-							if *record.Type == "A" || *record.Type == "AAAA" || *record.Type == "CNAME" {
-								domain := *record.Name
-								conn, err := tls.Dial("tcp", domain+":443", nil)
-								if err != nil {
-									fmt.Printf("Type: %v\nName: %v\nError: %v\n\n", *record.Type, *record.Name, err)
-								} else {
-									err = conn.VerifyHostname(domain)
+							if *record.Type == "A" {
+								arr[0].Targets = append(arr[0].Targets, *record.Name)
+							}
+							/*
+								fmt.Println(*record.Name)
+								if *record.Type == "A" || *record.Type == "AAAA" || *record.Type == "CNAME" {
+									domain := *record.Name
+									conn, err := tls.Dial("tcp", domain+":443", nil)
 									if err != nil {
 										fmt.Printf("Type: %v\nName: %v\nError: %v\n\n", *record.Type, *record.Name, err)
-										conn.Close()
+									} else {
+										err = conn.VerifyHostname(domain)
+										if err != nil {
+											fmt.Printf("Type: %v\nName: %v\nError: %v\n\n", *record.Type, *record.Name, err)
+											conn.Close()
+										}
 									}
 								}
-							}
+							*/
 						}
 						return *page.IsTruncated
 					})
 				if err != nil {
 					log.Println(err)
 				}
+/*
+				j, _ := json.Marshal(arr)
+				// fmt.Println(string(j))
+
+				f, err := os.OpenFile("targets.json", os.O_RDWR|os.O_CREATE, 0664)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				if _, err := f.WriteString(string(j)); err != nil {
+					f.Close()
+					log.Fatal(err)
+				}
+
+				if err := f.Close(); err != nil {
+					log.Fatal(err)
+				}
+*/
 			}
 			return *page.IsTruncated
 		})
 	if err != nil {
 		log.Println(err)
 	}
+
+	j, _ := json.Marshal(arr)
+	fmt.Println(string(j))
+
 }
